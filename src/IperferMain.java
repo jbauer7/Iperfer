e1826f21622c977e6bf4e535569824247372e8e4
Joseph Bauer <jwbauer3@wisc.edu>
2016-01-27 19:13:00 -0600

    format fix

--- src/IperferMain.java
+++ src/IperferMain.java
@@ -11,20 +11,20 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 public class IperferMain {
-	
-// Constants	
-final static String CLIENT_MODE = "-c";
-final static String SERVER_MODE = "-s";
-final static int SECONDS_2_NANO = 1000000000;
-final static int LOW_THRESHOLD_PORT = 1024;
-final static int HIGH_THRESHOLD_PORT = 65535;
-final static char[] outBuffer = new char[1000];
+
+	// Constants	
+	final static String CLIENT_MODE = "-c";
+	final static String SERVER_MODE = "-s";
+	final static int SECONDS_2_NANO = 1000000000;
+	final static int LOW_THRESHOLD_PORT = 1024;
+	final static int HIGH_THRESHOLD_PORT = 65535;
+	final static char[] outBuffer = new char[1000];
 
 
-    /**
- 	 * Main method: Iperfer tool sends packets using TCP to measure network 
- 	 * bandwidth
- 	 */
+	/**
+	 * Main method: Iperfer tool sends packets using TCP to measure network 
+	 * bandwidth
+	 */
 	public static void main(String[] args) {
 		if(args.length>0 && args[0].equals(CLIENT_MODE) && args.length==7){
 			client(args[2],Integer.parseInt(args[4]), Double.parseDouble(args[6]));
@@ -37,65 +37,64 @@ final static char[] outBuffer = new char[1000];
 			System.exit(-1);
 		}
 	}
-	
-	
+
+
 	/**
-     * Send data as quickly as possible to the server keeping track of packets sent
-     *
-     * @param  String hostname
-     * @param  int serverPort
-     * @param  double time (in seconds)
-     * @return void
-     */
+	 * Send data as quickly as possible to the server keeping track of packets sent
+	 *
+	 * @param  String hostname
+	 * @param  int serverPort
+	 * @param  double time (in seconds)
+	 * @return void
+	 */
 	private static void client(String hostname, int serverPort, double time){
-		long startTime;
-		
 		if(serverPort<LOW_THRESHOLD_PORT || serverPort>HIGH_THRESHOLD_PORT){
 			System.out.println("Error: port number must be in the range 1024 to 65535");
 			System.exit(-1);
 		}
-		
+
+		long startTime;
+
+
 		try{
-			    Socket mySocket = new Socket(hostname, serverPort);
-			    PrintWriter out = new PrintWriter(mySocket.getOutputStream(), true);
-			    
-				startTime=System.nanoTime();
-				while((System.nanoTime()-startTime) < time*SECONDS_2_NANO){
-				    out.write(outBuffer);
-				}  
-			    
-			    mySocket.close();
+			Socket mySocket = new Socket(hostname, serverPort);
+			PrintWriter out = new PrintWriter(mySocket.getOutputStream(), true);
+
+			startTime=System.nanoTime();
+			while((System.nanoTime()-startTime) < time*SECONDS_2_NANO){
+				out.write(outBuffer);
+			}  
+
+			mySocket.close();
 		}
 		catch(IOException e){
 			e.printStackTrace();
 		}
-		
-	
+
+
 		System.out.println("sent=6543 KB rate=5.234 Mbps");
 	}
-	
-	
+
+
 	/**
-     * Receive data from client as quickly as possible keeping track of packets received
-     *
-     * @param  int listenPort
-     * @return void
-     */
+	 * Receive data from client as quickly as possible keeping track of packets received
+	 *
+	 * @param  int listenPort
+	 * @return void
+	 */
 	private static void server(int listenPort){
-		
 		if(listenPort<LOW_THRESHOLD_PORT || listenPort>HIGH_THRESHOLD_PORT){
 			System.out.println("Error: port number must be in the range 1024 to 65535");
 			System.exit(-1);
 		}
-		
+
 		try{ 
-			    ServerSocket serverSocket = new ServerSocket(listenPort);
-			    Socket clientSocket = serverSocket.accept();			 
-			    BufferedReader in = new BufferedReader(
-			        new InputStreamReader(clientSocket.getInputStream()));
+			ServerSocket serverSocket = new ServerSocket(listenPort);
+			Socket clientSocket = serverSocket.accept();			 
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
 		}
-		catch(SocketTimeoutException s)
-		{
+		catch(SocketTimeoutException s){
 			System.out.println("Socket timed out!");
 			System.exit(-1);
 		}
